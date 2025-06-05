80d7b001cd4c6ab9c538e3ce752249cbc99eeaf7
Marvin <marvin@mail.pollo.ai>
2025-06-05 14:14:49 +0800

    feat: githublogin

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 
 import { db } from "@/server/db";
 
@@ -33,6 +34,7 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     DiscordProvider,
+    GithubProvider,
     /**
      * ...add more providers here.
      *
