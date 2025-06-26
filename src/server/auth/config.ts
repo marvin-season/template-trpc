054cb1932f9f127821435fd5c1c91298a01fb3e9
Marvin <marvin@mail.pollo.ai>
2025-06-26 15:09:14 +0800

    feat: 自定义登陆

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,7 +2,7 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import { type DefaultSession, type NextAuthConfig } from 'next-auth'
 import DiscordProvider from 'next-auth/providers/discord'
 import GithubProvider from 'next-auth/providers/github'
-
+import type { Provider } from 'next-auth/providers'
 import { db } from '@/server/db'
 
 /**
@@ -26,6 +26,19 @@ declare module 'next-auth' {
   //   // role: UserRole;
   // }
 }
+const providers: Provider[] = [DiscordProvider, GithubProvider]
+
+// 自定义登陆页面使用
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === 'function') {
+      const providerData = provider()
+      return { id: providerData.id, name: providerData.name }
+    } else {
+      return { id: provider.id, name: provider.name }
+    }
+  })
+  .filter((provider) => provider.id !== 'credentials')
 
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
@@ -33,19 +46,19 @@ declare module 'next-auth' {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  providers: [
-    DiscordProvider,
-    GithubProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+  /**
+   * ...add more providers here.
+   *
+   * Most other providers require a bit more work than the Discord provider. For example, the
+   * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+   * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+   *
+   * @see https://next-auth.js.org/providers/github
+   */
+  providers,
+  pages: {
+    signIn: '/login',
+  },
   adapter: PrismaAdapter(db),
   callbacks: {
     session: ({ session, user }) => ({
--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,10 +1,10 @@
 import NextAuth from 'next-auth'
 import { cache } from 'react'
 
-import { authConfig } from './config'
+import { authConfig, providerMap } from './config'
 
 const { auth: uncachedAuth, handlers, signIn, signOut } = NextAuth(authConfig)
 
 const auth = cache(uncachedAuth)
 
-export { auth, handlers, signIn, signOut }
+export { auth, handlers, signIn, signOut, providerMap }
