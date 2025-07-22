be220b2a992dbed084c612ab49da3cff41995514
Marvin <marvin@mail.pollo.ai>
2025-07-22 21:15:13 +0800

    fix: pdf with node_modules

--- src/components/PDFView/index.tsx
+++ src/components/PDFView/index.tsx
@@ -3,7 +3,10 @@
 import { Document, Page, pdfjs } from 'react-pdf'
 import { useState } from 'react'
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString()
 
 export default function PDFViewer() {
   const [allPages, setAllPages] = useState<number[]>([])
