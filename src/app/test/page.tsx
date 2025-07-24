437fc372a5da2c65befd75538024128865f6bede
Marvin <marvin@mail.pollo.ai>
2025-07-24 16:56:20 +0800

    feat: Progress 组件

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,32 +1,29 @@
 'use client'
 
-import { useTRPC } from '@/trpc/react'
-import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Progress } from '@/components/ui'
 
 export default function TestPage() {
-  const [id, setId] = useState(1)
-  const trpc = useTRPC()
-  const { data, status, refetch } = useQuery(
-    trpc.post.get.queryOptions(id, {
-      enabled: !!id,
-      staleTime: 0,
-    }),
-  )
-  console.log('status', status)
-  const { data: list } = useQuery(trpc.post.list.queryOptions())
+  const [progress, setProgress] = useState(40)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prev) => {
+        const next = prev + 0.1
+        if (next > 100) {
+          clearInterval(timer)
+          return 100
+        }
+        return next
+      })
+    }, 50)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
   return (
-    <div>
-      {list?.map((item) => (
-        <div
-          key={item.id}
-          onClick={() => {
-            setId(item.id)
-          }}
-        >
-          {item.name} {item.id}
-        </div>
-      ))}
+    <div className='flex gap-2'>
+      <Progress value={progress} className='w-[60%]' /> {progress}
     </div>
   )
 }
