3e811ffc22754efbe0a33fbaa12d8c89c3b4f5f2
Marvin <marvin@mail.pollo.ai>
2025-07-30 14:56:11 +0800

    style: 样式优化

--- src/app/flow.ai/_components/workflow/nodes/panel/javascript-panel/panel.tsx
+++ src/app/flow.ai/_components/workflow/nodes/panel/javascript-panel/panel.tsx
@@ -1,14 +1,18 @@
 import { Properties } from '@flow.ai/_components/workflow/nodes/components/properties'
 import { devLog } from '@/utils/common'
 import { type NodeProps } from 'reactflow'
+import { Code } from 'lucide-react'
 
 export function JavaScriptPanel(props: NodeProps) {
   const { data } = props
   devLog(data)
   return (
     <div>
-      JavaScriptPanel
-      <div className='max-h-[200px] overflow-y-auto px-4'>
+      <div className='flex items-center justify-between border-b px-4'>
+        <Code className='size-4' />
+        <span className='text-yellow-300'>{'JS'}</span>
+      </div>
+      <div className='mt-2 max-h-[200px] overflow-y-auto p-4'>
         <Properties properties={data.properties} name={data.constructor.name} />
       </div>
     </div>
