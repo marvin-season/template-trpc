948ab959818f7ab440bb2a9bfd043b4a604758a3
Marvin <marvin@mail.pollo.ai>
2025-07-30 14:29:50 +0800

    refactor: 结构

--- src/app/flow.ai/_components/workflow/nodes/panel/constants.ts
+++ src/app/flow.ai/_components/workflow/nodes/panel/constants.ts
@@ -1,8 +1,9 @@
-import { StartPanel, EndPanel, IfElsePanel } from '.'
+import { StartPanel, EndPanel, IfElsePanel, JavaScriptPanel } from '.'
 import type { ComponentType } from 'react'
 
 export const PanelComponentMap: Record<string, ComponentType<any>> = {
   'start': StartPanel,
   'end': EndPanel,
   'if-else': IfElsePanel,
+  'javascript': JavaScriptPanel,
 }
--- src/app/flow.ai/_components/workflow/nodes/panel/index.ts
+++ src/app/flow.ai/_components/workflow/nodes/panel/index.ts
@@ -1,4 +1,4 @@
-export * from './JavaScriptPanel'
+export * from './javascript-panel/panel'
 export * from './start/panel'
 export * from './if-else/panel'
 export * from './end/panel'
