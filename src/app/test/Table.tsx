c5607b57b2f100a21cc3a5b81d9d9aeb42108440
Marvin <marvin@mail.pollo.ai>
2025-07-31 20:00:43 +0800

    feat: invalidateQueries

--- src/app/test/Table.tsx
+++ src/app/test/Table.tsx
@@ -2,20 +2,29 @@
 
 import { Button } from '@/components/ui'
 import { useTRPC } from '@/trpc/react'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 export function Table() {
   const trpc = useTRPC()
-  const { data, isLoading, isFetching, refetch } = useQuery(
-    trpc.post.list.queryOptions(undefined, {
-      enabled: false,
-    }),
-  )
+  const queryClient = useQueryClient()
+  const options = trpc.post.list.queryOptions(undefined, {
+    enabled: false,
+    staleTime: 10000,
+  })
+  const { data, isLoading, isFetching, refetch } = useQuery(options)
 
   return (
     <div>
       客户端渲染
-      <Button disabled={isFetching} onClick={() => refetch()}>
+      <Button
+        disabled={isFetching}
+        onClick={() => {
+          queryClient.invalidateQueries({
+            queryKey: options.queryKey,
+          })
+          refetch()
+        }}
+      >
         {isFetching ? 'loading...' : 'refetch'}
       </Button>
       {JSON.stringify(data)}
