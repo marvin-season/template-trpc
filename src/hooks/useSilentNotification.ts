5e3a8a8ba9fdc8bf01d651bbb0a2bb9ed11c2427
Marvin <marvin@mail.pollo.ai>
2025-09-04 10:34:44 +0800

    feat: 移除web push 前端 sw

---
+++ src/hooks/useSilentNotification.ts
@@ -0,0 +1,65 @@
+import {
+  notification,
+  registerServiceWorker,
+  type IMessage,
+  type IOptions,
+} from '@/utils/notification'
+import { useLocalStorageState } from 'ahooks'
+import { useCallback, useEffect, useMemo } from 'react'
+
+registerServiceWorker('sw.js')
+
+export function useSilentNotification(message: IMessage, options: IOptions) {
+  const { wait } = options
+  const [silentNotification, setSilentNotification] = useLocalStorageState(
+    'silent-notification',
+    {
+      defaultValue: {
+        lastNotificationTime: 0, // 上次通知客户端时间戳
+        lastNotificationTimeForDebug: new Date().toLocaleString(), // 上次通知客户端时间戳
+      },
+    },
+  )
+
+  const passedTime = useMemo(() => {
+    if (silentNotification?.lastNotificationTime) {
+      return Date.now() - (silentNotification.lastNotificationTime || 0)
+    }
+
+    return 0
+  }, [silentNotification])
+
+  const sendNotification = useCallback(
+    async (msg?: IMessage) => {
+      await notification({ ...message, ...msg })
+      setSilentNotification({
+        lastNotificationTime: Date.now(),
+        lastNotificationTimeForDebug: new Date().toLocaleString(),
+      })
+    },
+    [setSilentNotification, message],
+  )
+
+  // 轮询机制
+  useEffect(() => {
+    registerServiceWorker('sw.js')
+    // sendNotification()
+
+    const intervalTime = wait - passedTime < 0 ? wait : wait - passedTime
+    let timer: ReturnType<typeof setInterval>
+    console.log({ intervalTime, passedTime })
+
+    if (intervalTime > 0) {
+      timer = setInterval(async () => {
+        console.log('dddd', intervalTime)
+        await sendNotification()
+      }, intervalTime)
+    }
+
+    return () => clearInterval(timer)
+  }, [sendNotification, wait, passedTime])
+
+  return {
+    sendNotification,
+  }
+}
