a2a3834dd9f4efcaf38af885272d7c088f851701
Marvin <marvin@mail.pollo.ai>
2025-09-08 14:45:31 +0800

    站内通知

--- src/app/test/FCM/index.tsx
+++ src/app/test/FCM/index.tsx
@@ -1,8 +1,33 @@
 'use client'
-import useFCM from '@/hooks/useFCM'
+
+import useFCMToken from '@/hooks/useFCMToken'
+import { onMessage } from 'firebase/messaging'
+import { messaging } from '@/utils/firebase'
+import { toast } from 'sonner'
+import { useEffect } from 'react'
 
 export default function FCM() {
-  const { token } = useFCM()
+  const token = useFCMToken()
+
+  useEffect(() => {
+    if (token) {
+      const un = onMessage(messaging(), (payload) => {
+        const { notification } = payload
+        console.log('notification', notification)
+        toast.info(
+          <>
+            <div className='font-bold'>{payload.notification?.title}</div>
+            <div className='text-sm'>{payload.notification?.body}</div>
+          </>,
+        )
+      })
+
+      return () => {
+        un()
+      }
+    }
+  }, [token])
+
   return (
     <div className=''>
       <div>{'token: ' + token}</div>
