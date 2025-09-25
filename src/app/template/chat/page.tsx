2881ed45c1a78606a787859bcf5770dba52ae22d
Marvin <marvin@mail.pollo.ai>
2025-09-25 20:42:32 +0800

    feat: 更新 figma icon

--- src/app/template/chat/page.tsx
+++ src/app/template/chat/page.tsx
@@ -4,7 +4,7 @@ export default function ChatPage() {
       <span className='iconify mdi-light--home'></span>
       <span className='iconify-color vscode-icons--file-type-tailwind'></span>
       <span className='i-[mdi--home]'></span>
-      <span className='i-cus--ai-writer'></span>
+      <span className='i-cus--deep-research size-12 text-blue-400'></span>
     </div>
   )
 }
