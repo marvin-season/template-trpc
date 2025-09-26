67fab2744198ee8280d767f05bcd5950f3996243
Marvin <marvin@mail.pollo.ai>
2025-09-26 14:33:24 +0800

    feat: add icon

--- src/app/template/chat/page.tsx
+++ src/app/template/chat/page.tsx
@@ -5,6 +5,7 @@ export default function ChatPage() {
       <span className='iconify-color vscode-icons--file-type-tailwind'></span>
       <span className='i-[mdi--home]'></span>
       <span className='i-cus--deep-research size-12 text-blue-400'></span>
+      <span className='i-cus--arrow-up text-blue-400'></span>
     </div>
   )
 }
