9ee964474af117ef5098e2e026bb892b1cdd7042
Marvin <2764876579@qq.com>
2025-11-04 09:17:18 +0800

    feat: remove code dowhat

--- src/app/dowhat/_components/index.ts
+++
@@ -1,4 +0,0 @@
-export { WheelDraw } from './WheelDraw'
-export { SlotDraw } from './SlotDraw'
-export { AnimatedCard } from './AnimatedCard'
-export { Confetti } from './Confetti'
--- src/app/dowhat/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className='h-screen overflow-y-scroll'>{children}</div>
-}
